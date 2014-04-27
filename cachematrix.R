b7825d96ebd7366b26221379b4d77f46c8ab8df8
asd999kv <asd999kv@gmail.com>
2014-04-27 15:39:20 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -21,13 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-     inv_x <- x$getinverse()
+    ## Return a matrix that is the inverse of 'x'
+    inv_x <- x$getinverse()
     if (!is.null(inv_x)) {
-        message("getting cached inverse matrix")
+        message("cached inverse matrix")
         return(inv_x)
     } else {
         inv_x <- solve(x$get())
         x$setinverse(inv_x)
         return(inv_x)
+    }
 }
